1c30c12f544393b4d777e4fd79d0cd6bc3d36fa7
Joan A. Jagelski <J.JAGELSKI@labs.clark.edu>
2019-02-19 12:49:37 -0800

    class work

--- page.php
+++ page.php
@@ -10,16 +10,23 @@
     <title>Page Title</title>
 </head>
 <body>
-<div class="container">
-    <section>
-        <h1>Evergreen</h1>
+ <div class="container"> <!--container-fluid -->
+    <section class="bg-success text-white p-5"> <!--bg-warning, success, info-->
+        <img src="img/200px-clark_College_logo.png" class="d-block mx-auto" alt="">
+        <h1 class="display-4">Evergreen</h1> <!-- class="display-1" can go in a div and goes 1thru 4-->
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates at reiciendis expedita adipisci sint? Esse quas repellendus obcaecati dicta. Iusto voluptates dicta dolor omnis necessitatibus labore amet voluptatem distinctio repudiandae.</p>
-        <h2>Camas</h2>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius enim, minus cumque asperiores libero consectetur. Praesentium sint itaque, eaque assumenda expedita totam dignissimos molestias sequi natus placeat, aliquid sapiente atque.</p>
-        <h3>Vanmcouver</h3>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto sit ad, aspernatur autem temporibus et ipsam, consequatur excepturi delectus, quo ratione! Quam explicabo repudiandae commodi delectus saepe, adipisci laborum quaerat!</p>
-        <h4>Comsewogue</h4>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam dolores eos quasi nostrum ducimus nam pariatur molestias consequuntur mollitia recusandae illo accusantium laboriosam debitis minus, quia, cum omnis. Quod, et!</p>
+        <h2 class="h1">Camas</h2><!--to keep your structure but change the size of <h2> tag, add class="h1"-->
+        <p class="h1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius enim, minus cumque asperiores libero consectetur. Praesentium sint itaque, eaque assumenda expedita totam dignissimos molestias sequi natus placeat, aliquid sapiente atque.</p>
+        <h3>Vancouver</h3>
+        <!--class="lead" changes the text spacing to look more inportant. aleading paragraph.-->
+        <p class="lead">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto sit ad, aspernatur autem temporibus et ipsam, consequatur excepturi delectus, quo ratione! Quam explicabo repudiandae commodi delectus saepe, adipisci laborum quaerat!</p>
+        <!-- mt = margin-top, mb = margin-bottom, mx = rigth,left my = topand bottom, m = all margins and goes 1-5 -->
+        <!-- for margin: mx = horizontal  and my = vertical based on the x/y axis. -->
+        <h4 class="mt-4">Comsewogue</h4>
+        <!-- class="text-left" class="text-right" class="text-justify" class="text-center" -->
+        <!-- can add another class to the <p class="text-justify text-uppercase"> -->
+        <p class="text-justify text-uppercase">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam dolores eos quasi nostrum ducimus nam pariatur molestias consequuntur mollitia recusandae illo accusantium laboriosam debitis minus, quia, cum omnis. Quod, et!</p>
+        <p class="blockquote border-left pl-4 font-italic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat veritatis minima, quidem earum porro voluptatem perferendis, vitae est consectetur eos facere eveniet magni mollitia ea et labore illo alias recusandae.</p>
     </section>
 </div>
 
@@ -28,4 +35,4 @@
 <!-- Bootstrap JavaScript -->
 <script src="js/bootstrap.min.js"></script>
 </body>
-</html>
+</html> 
